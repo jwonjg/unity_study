542fa9ca200038b7dd27110d61845bc0d12a27cb
jjw <jwonjg@naver.com>
2017-01-25 00:42:16 +0900

    no message

---
+++ ToEarthGame/j_Scripts/ObstacleManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour {
+
+    public GameObject[] obstacles;
+    public int obstacleCount;
+
+    private float cameraWidth;
+    private float cameraHeight;
+
+    // Use this for initialization
+    void Start () {
+
+        // Debug.Log(Camera.main.orthographicSize);
+        // Debug.Log("transform.localPosition.y:" + transform.localPosition.y);
+
+        cameraWidth = Camera.main.orthographicSize * Camera.main.aspect * 2f;
+        cameraHeight = Camera.main.orthographicSize * 2f;
+
+        for(int i = 0; i < obstacleCount; i++)
+        {
+            float x = Random.Range(-Camera.main.orthographicSize * Camera.main.aspect, Camera.main.orthographicSize * Camera.main.aspect);
+            float y = Random.Range(-Camera.main.orthographicSize, Camera.main.orthographicSize);
+            Instantiate(obstacles[0], new Vector3(x, y), transform.rotation);
+        }
+
+        // Instantiate(obstacles[0], new Vector3(Camera.main.orthographicSize * Camera.main.aspect, Camera.main.orthographicSize), transform.rotation);
+        // Instantiate(obstacles[1], transform.localPosition, transform.rotation);
+        // Debug.Log("transform.rotation:" + transform.rotation);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
---
+++ ToEarthGame/j_Scripts/Player.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public float speed;             //Floating point variable to store the player's movement speed.
+    // private Animator[] anims;
+    private Animator leftFireAnim;
+    private Animator rightFireAnim;
+    private Animator playerAnim;
+
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    public Text cntText;
+    public Text winText;
+    private int countHint;
+    private int totalHint;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+        // anims = GetComponentsInChildren<Animator>();
+
+        // 자식 GameObject를 찾아 각각에 설정된 Animator 컴포넌트를 얻음
+        // TODO: 각 GameObject 하위 스크립트로 분리하는 방향도 검토할 것
+        leftFireAnim = transform.Find("LeftFire").GetComponent<Animator>();
+        rightFireAnim = transform.Find("RightFire").GetComponent<Animator>();
+        playerAnim = GetComponent<Animator>();
+
+        totalHint = GameObject.FindGameObjectsWithTag("HintTag").Length;
+        Debug.Log(totalHint);
+        winText.text = "";
+        countHint = 0;
+        setHintText();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce(movement * speed);
+
+        leftFireAnim.SetFloat("Speed", rb2d.velocity.SqrMagnitude());
+        rightFireAnim.SetFloat("Speed", rb2d.velocity.SqrMagnitude());
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rightFireAnim.SetBool("Right", true);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            rightFireAnim.SetBool("Right", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            leftFireAnim.SetBool("Left", true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            leftFireAnim.SetBool("Left", false);
+        }
+
+        /*
+        foreach(Animator anim in anims)
+        {
+            anim.SetFloat("Speed", rb2d.velocity.SqrMagnitude());
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                anim.SetBool("Right", true);
+            }
+            if (Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                anim.SetBool("Right", false);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                anim.SetBool("Left", true);
+            }
+            if (Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                anim.SetBool("Left", false);
+            }
+        }
+        */
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("HintTag")) {
+            collision.gameObject.SetActive(false);
+            countHint++;
+            setHintText();
+        } else if (collision.gameObject.CompareTag("ObstacleTag"))
+        {
+            playerAnim.SetTrigger("DieTrigger");
+        }
+    }
+
+    void onDieAnimationEnd()
+    {
+        // TODO: 게임 종료 시 
+        gameObject.SetActive(false);
+        setGameOverText();
+    }
+
+    void setHintText()
+    {
+        cntText.text = "hint : " + countHint.ToString();
+        if (countHint == totalHint)
+        {
+            winText.text = "complete!";
+        }
+    }
+
+    void setGameOverText()
+    {
+        winText.text = "game over!";
+    }
+}
