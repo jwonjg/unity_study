c688d775aeefe2c0625ec5ab21c29b03c9287c80
Kiwon Jeong <kiwonjeong@Kiwonui-MacBook-Pro.local>
2017-02-03 08:05:12 -0800

    4주차

--- ToEarthGame/k_Scripts/CameraController.cs
+++ ToEarthGame/k_Scripts/CameraController.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public GameObject player;
-
+    private Animator leftHandleAnim;
+    private Animator rightHandleAnim;
     private Vector3 offset;
     
 	// Use this for initialization
 	void Start () {
         offset = transform.position - player.transform.position;
+        leftHandleAnim = transform.Find("leftHand").GetComponent<Animator>();
+        rightHandleAnim = transform.Find("rightHand").GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+//        Debug.Log(moveHorizontal);
+        if (moveHorizontal > 0) {
+            rightHandleAnim.SetFloat("Pressed", moveHorizontal);
+        } else if(moveHorizontal < 0) {
+            leftHandleAnim.SetFloat("Pressed", moveHorizontal);            
+        } else {
+            rightHandleAnim.SetFloat("Pressed", moveHorizontal);
+            leftHandleAnim.SetFloat("Pressed", moveHorizontal);  
+        } 
     }
 
     // runs after procedural animation and gathering last known states
--- ToEarthGame/k_Scripts/Player.cs
+++ ToEarthGame/k_Scripts/Player.cs
@@ -6,21 +6,41 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float speed;             //Floating point variable to store the player's movement speed.
+    // private Animator[] anims;
+    private Animator leftFireAnim;
+    private Animator rightFireAnim;
+    private Animator playerAnim;
+
     private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
 
-    private int count; //먹은 힌트 수
-    public Text countText;
+    public Text cntText;
     public Text winText;
-    private Animator[] anim;
+    private int countHint;
+    private int totalHint;
+
+    private bool leftPressed;
+    private bool rightPressed;
+    private PressCheck left;
+    private PressCheck right;
 
     // Use this for initialization
     void Start() {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
         rb2d = GetComponent<Rigidbody2D>();
-        anim = GetComponentsInChildren<Animator>();
-        count = 0;
+
+        leftFireAnim = transform.Find("leftFire").GetComponent<Animator>();
+        rightFireAnim = transform.Find("rightFire").GetComponent<Animator>();
+        playerAnim = GetComponent<Animator>();
+
+        totalHint = GameObject.FindGameObjectsWithTag("Hint").Length;
         winText.text = "";
+        countHint = 0;
         setCountText();
+
+        GameObject leftCheck = GameObject.Find("leftHand");
+        GameObject rightCheck = GameObject.Find("rightHand");
+        left = leftCheck.GetComponent <PressCheck> ();
+        right = rightCheck.GetComponent <PressCheck> ();
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
@@ -36,8 +56,36 @@ public class Player : MonoBehaviour {
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce(movement * speed);
+//        Debug.Log(moveHorizontal);
+        leftPressed = left.pressed;
+        rightPressed = right.pressed;
+        Debug.Log("left:" + leftPressed);
+        Debug.Log("right:" + rightPressed);
+        if(rightPressed || leftPressed) {
+        	rb2d.AddForce(movement * speed);
+        }
 
+        leftFireAnim.SetFloat("Speed", rb2d.velocity.SqrMagnitude());
+        rightFireAnim.SetFloat("Speed", rb2d.velocity.SqrMagnitude());
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rightFireAnim.SetBool("Right", true);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            rightFireAnim.SetBool("Right", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            leftFireAnim.SetBool("Left", true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            leftFireAnim.SetBool("Left", false);
+        }
+/*
         if (moveHorizontal > 0) {
             anim[2].SetFloat("Pressed", moveHorizontal);
         } else if(moveHorizontal < 0) {
@@ -50,22 +98,31 @@ public class Player : MonoBehaviour {
         Debug.Log(moveHorizontal);
 
         anim[0].SetFloat("Speed", rb2d.velocity.SqrMagnitude());
+*/
     }
 
     void OnTriggerEnter2D (Collider2D other) {
         if (other.gameObject.CompareTag ("Hint")) {
             other.gameObject.SetActive (false);
-            count++;
+            countHint++;
             setCountText();
+        } else if (other.gameObject.CompareTag ("Obstacle")) {
+            playerAnim.SetTrigger("DieTrigger");
         }
+
     }
 
     //update the current count string
     void setCountText() {
-        countText.text = "Hint: " + count.ToString();
-        if (count >= 4) {
+        cntText.text = "Hint: " + countHint.ToString();
+        if (countHint == totalHint) {
         	winText.text = "Completed";
         }
+    }
+
+    void onDieAnimationEnd() {
+        gameObject.SetActive(false);
+        winText.text = "Game Over";
     }
 
 }
